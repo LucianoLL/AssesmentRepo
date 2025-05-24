767d669cc04d2ebb4a1aacdefbb8aa93e3a8a707
Luciano Loma Lorenzana <lucianolomalor251@gmail.com>
2025-05-24 15:10:52 -0700

    Worked on the ProductCards, changed the style a bit to be more wider and zoomed in images

--- assesment/app/components/ProductCard.js
+++ assesment/app/components/ProductCard.js
@@ -8,54 +8,38 @@ https://nextjs.org/docs/pages/building-your-application/routing/linking-and-navi
 import Link from 'next/link';
 import {
     Card, 
-    CardContent, 
-    CardHeader, 
-    CardMedia,
     Grid,
-    Typography,
 } from '@mui/material';
 
 export default function ProductCard({id, image, name, price}) {
     const fixedPrice = price.toFixed(2);
     return(
+        // House entire Grid and Card in a Link component
         <Link href={`./product/${id}`}>
-            <Grid className='
-                    hover:shadow-xl/30 
-                    shadow-blue-500 rounded-md
-            '>
-                <Card className='
-                    rounded-md
-                    border-6
-                    border-transparent 
-                    hover:border-blue-500
-                    w-75
-                '>
-                    {
-                        image && 
-                        <CardMedia
-                            component={"img"}
-                            image={image}
-                        />
-                    }
-                    <CardContent>
-                        <Typography sx={{
-                            fontFamily: "sans-serif",
-                            fontWeight: "bold",
-                            }}
-                            variant='h5'
-                        >
-                            {name}
-                        </Typography>
-                        <Typography sx={{
-                            color: "blue",
-                            font: "sans-serif",
-                            fontWeight: "bold",
-                            fontSize: "14"
-                        }}>
-                            ${fixedPrice}    
-                        </Typography>
-                        
-                    </CardContent>
+            <Grid>
+                {/* 
+                Gave the Card component the set dimensions if 75x70, trying to create 
+                a subtle rectangle
+                */}
+                <Card className='w-75 h-70'>
+                    {/* Set the image height to 3/4 of the space, with hiding overflow */}
+                    <div className='overflow-hidden h-3/4 justify-center'>
+                        {
+                            image && 
+                            // Gave an image a wifth of 75, and used negative margins
+                            <img
+                                className='w-75 -mt-10'
+                                src={image}
+                            />
+                        }
+                    </div>
+                    {/* Basic text for the product details */}
+                    <div className='p-3 font-sans font-bold'>
+                        <p>{name}</p>
+                        <p className='text-blue-400'>${fixedPrice}</p>
+
+                    </div>
+                    
                 </Card>
             </Grid>
         </Link>
